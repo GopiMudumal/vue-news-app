068942937ce6ef6ce9a6fee5ef2171dcd8efa295
GopiMudumal <gopi.mudumal@perennialsys.com>
2025-01-21 11:21:58 +0530

    add dummy page

---
+++ src/views/DummyPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1 class="title"> Dummy page</h1>
+        <NewsContainer />
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.title {
+    margin-top: 180px;
+}
+</style>
